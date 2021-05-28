c81757a57a6139c80bdcb32d484f8b2a0b78ece1
Brennan Fife <brenfife@gmail.com>
2021-05-28 10:23:19 -0500

    init commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reactStrictMode: true,
+  assetPrefix: '.',
+  env: {
+    INFURA_PROJECT_ID: 'a4e978103d87452881d1ea298948aa42',
+  },
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+
+export default function App({ Component }) {
+  return (
+    <ChakraProvider>
+      <Component />
+    </ChakraProvider>
+  );
+}
