0b7eb7a10aeb3a95aafaacf2aa7eab3958346bd8
Brennan Fife <brenfife@gmail.com>
2021-05-28 13:06:24 -0500

    add header component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,11 @@
 import { Box } from '@chakra-ui/react';
 import { ReactNode } from 'react';
+import Header from '../components/Header';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <Box>
+      <Header />
       <Box mb="2rem">{children}</Box>
     </Box>
   );
