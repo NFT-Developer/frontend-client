57c7b86d9bf1e6501ccb62e179c014214238b048
Brennan Fife <brenfife@gmail.com>
2021-05-30 11:15:44 -0500

    typo

--- src/components/Map/Cryptovoxels/Map.tsx
+++ src/components/Map/Cryptovoxels/Map.tsx
@@ -1,8 +1,21 @@
 // import * as ReactLeaflet from 'react-leaflet';
 // import 'leaflet/dist/leaflet.css';
 // const { MapContainer } = ReactLeaflet;
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react';
 
 export default function Map({ children, ...rest }) {
   // return <MapContainer {...rest}>{children(ReactLeaflet)}</MapContainer>;
-  return <h1>code not load map</h1>;
+
+  <Alert status="error" variant="left-accent">
+    <AlertIcon />
+    <AlertTitle mr={2}>Could not load map.</AlertTitle>
+    <AlertDescription>
+      Please reload your browser and try again!
+    </AlertDescription>
+  </Alert>;
 }
